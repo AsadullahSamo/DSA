e34caf25e47ad2c36405430a974ca43f187cfac2
Asad <asadsamoon8@gmail.com>
2023-03-12 02:36:13 +0500

    Update Project

--- DSA Project/src/Project.java
+++ DSA Project/src/Project.java
@@ -8,16 +8,17 @@ public class Project {
 
         //     Step 1
         Step1 step1 = new Step1();
+        step1.getAllCountriesForYear();
 
 //        step1.getAllCountriesForYear();
 
         //     Problem 2:     Biggest earthquakes from 2005 to 2015
         Step2 step2 = new Step2();
-        step2.getLargestMagnitudeForEachCountry();
+//        step2.getLargestMagnitudeForEachCountry();
 
         //     Problem 3:     Recent five earthquakes
         Step3 step3 = new Step3();
-        step3.getDetailsOfCountry();
+//        step3.getDetailsOfCountry();
 
 //        int mostVulnerableCountry = step3.mostNumberOfEarthquakes();
 //        System.out.println(mostVulnerableCountry);
